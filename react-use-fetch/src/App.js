cd4e43764853c2d2c351d2d3679c6e7e67989386
unknown <ignatius.pratama@inticorp.tech>
2022-07-16 13:53:26 +0700

    init-commit

---
+++ react-use-fetch/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div >
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+    </div>
+  );
+}
+
+export default App;
